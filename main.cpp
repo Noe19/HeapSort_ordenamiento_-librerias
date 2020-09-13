ff85978120d9e856caeb7f35e785c2cdaaa24c3b
denisse cumbal <denissecumbal2015@hotmail.com>
2020-09-13 17:49:40 +0000

    hice una pequeña descripción de  programa y lo que necesita para copilar el mismo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "heapSort.h"
-
+//para la ejecusion del Orden_heapSort debemos tener la libreria heapSort donde se almacenas las funciones de ingresar datos y ordenarlos
 using namespace std;
 int main() {
  Ingresar_heapSort();
